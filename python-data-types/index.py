360e6e5cf8f2396f59e95b1ee8d195d549eb702a
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-02-26 21:08:07 +0330

    python data types

---
+++ python-data-types/index.py
@@ -0,0 +1,15 @@
+x = "Hello World"		
+x = 20		
+x = 20.5		
+x = 1j		
+x = ["apple", "banana", "cherry"]		
+x = ("apple", "banana", "cherry")		
+x = range(6)		
+x = {"name" : "John", "age" : 36}		
+x = {"apple", "banana", "cherry"}		
+x = frozenset({"apple", "banana", "cherry"})		
+x = True		
+x = b"Hello"		
+x = bytearray(5)		
+x = memoryview(bytes(5))		
+x = None
