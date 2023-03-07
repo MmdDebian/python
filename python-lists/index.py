44d989965fbff0583ecc3a70db3be8918fb17887
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-03-07 21:22:43 +0330

    python lists

--- python-lists/index.py
+++ python-lists/index.py
@@ -5,28 +5,22 @@ print(usernameLists)
 # print list length 
 print(len(usernameLists))
 
-
 print(type(usernameLists))
-
 
 # boolean lists 
 booleanLists = [True , False , False , True]
 
 print(booleanLists)
 
-
 # boolean string and number list : 
-
 lists = [True , 15 , "mohamad"]
 
 print(lists)
 
 # use the list constructor 
-
 laptops = list(('apple' , 'asus' , 'lenovo' , 'msi'))
 
 print(laptops)
-
 
 # print list item  
 print(laptops[1])
