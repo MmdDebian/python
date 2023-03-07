3d6675712190058402f6f22fecf569fa09af8769
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-03-07 20:55:40 +0330

    python lists

---
+++ python-lists/index.py
@@ -0,0 +1,32 @@
+# lists 
+usernameLists = ['mohamad','milad','erfan']
+print(usernameLists)
+
+# print list length 
+print(len(usernameLists))
+
+
+print(type(usernameLists))
+
+
+# boolean lists 
+booleanLists = [True , False , False , True]
+
+print(booleanLists)
+
+
+# boolean string and number list : 
+
+lists = [True , 15 , "mohamad"]
+
+print(lists)
+
+# use the list constructor 
+
+laptops = list(('apple' , 'asus' , 'lenovo' , 'msi'))
+
+print(laptops)
+
+
+# print list item  
+print(laptops[1])
